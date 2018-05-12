9d94a0ce8bd6235450ac55d0fb8c536406e2b528
Hassen <hg@evaste.com>
2018-05-12 20:39:10 +0100

    update

--- hassen_fileuploader.js
+++ hassen_fileuploader.js
@@ -7,22 +7,32 @@ function hfu(config){
 			'drophereText' : 'Glissez vos fichiers ici',
 		},
 	}
-  var hfuFileinputPrototype = document.querySelector(config.hfuFileinputPrototype);
-  if(!hfuFileinputPrototype.classList.contains("hfuFileinputPrototype")){//targetted file input does NOT  have the 'hfuFileinputPrototype' class ?
-    hfuFileinputPrototype.classList.add("hfuFileinputPrototype");
-  }
-  hfuFileinputPrototype.setAttribute("name","hfuFileinputPrototype");
+   	var hfuFileinputPrototype = document.querySelector(config.hfuFileinputPrototype);
+  	if(!hfuFileinputPrototype.classList.contains("hfuFileinputPrototype")){//targetted file input does NOT  have the 'hfuFileinputPrototype' class ? add it
+    	hfuFileinputPrototype.classList.add("hfuFileinputPrototype");
+	}
+	if(hfuFileinputPrototype.multiple){//targetted file has the multiple attribute ?
+  		hfuFileinputPrototype.setAttribute("name","hfuFileinputPrototype[]");
+  	}
+    else{
+  		hfuFileinputPrototype.setAttribute("name","hfuFileinputPrototype");
+  	}
 
-  var hfuContainerDiv = document.createElement("div");
-  var hfuAbsoluteDiv = document.createElement("div");
-  var hfuDropHere =  document.createElement("h3");
+	////START - building elements
+  	var hfuContainerDiv = document.createElement("div");
+  	var hfuAbsoluteDiv = document.createElement("div");
+  	var hfuDropHere =  document.createElement("h3");
 
-  if(config.lang && Object.keys(texts).indexOf(config.lang) != -1){ //lang chosen for the hfuDropHere ?
-  	hfuDropHere.textContent = texts[config.lang].drophereText;
-  }
-  else{// english default for the hfuDropHere
-	hfuDropHere.textContent = texts.en.drophereText;
-  }
+  	//START - setting texts
+  	if(config.lang && Object.keys(texts).indexOf(config.lang) != -1){ //lang chosen for the hfuDropHere ?
+  		hfuDropHere.textContent = texts[config.lang].drophereText;
+		hfuFileinputPrototype.setAttribute("title", texts[config.lang].drophereText);
+  	}
+  	else{// english default for the hfuDropHere
+		hfuDropHere.textContent = texts.en.drophereText;
+		hfuFileinputPrototype.setAttribute("title", texts.en.drophereText);
+  	}
+  //END - setting texts
 
   hfuContainerDiv.classList.add("hfuContainerDiv");
   hfuAbsoluteDiv.classList.add("hfuAbsoluteDiv");
@@ -35,7 +45,6 @@ function hfu(config){
 	 hfuDropHere.classList.add(config.hfuDropHereClass);
   }
 
-
   hfuAbsoluteDiv.appendChild(hfuDropHere);
   hfuContainerDiv.appendChild(hfuAbsoluteDiv);
   hfuContainerDiv.appendChild(hfuFileinputPrototype.cloneNode(true));
@@ -43,15 +52,15 @@ function hfu(config){
   hfuFileinputPrototype.parentNode.insertBefore(hfuContainerDiv,hfuFileinputPrototype);
   hfuFileinputPrototype.parentNode.removeChild(hfuFileinputPrototype);
   hfuFileinputPrototype = document.querySelector(config.hfuFileinputPrototype);
-
-  
+  ////END building elements
 
   hfuFileinputPrototype.addEventListener("change", function(){
     console.log("input changed !");
     console.log(this);
     console.log(this.files);
   });
-//START events
+
+  //START events
   document.addEventListener("drop", function(event){
     if (!event.target.isSameNode(hfuFileinputPrototype)) {//drop NOT happened on the drop file zone ?
       //event.preventDefault();
@@ -59,11 +68,11 @@ function hfu(config){
     else{
 	  console.log("drop !");
 
-	  if(hfuDropHere.classList.contains('hfuDropHereFocus')){
+	  if(hfuDropHere.classList.contains('hfuDropHereFocus')){//remove .hfuDropHereFocus from hfuDropHere after DROP
 	  	hfuDropHere.classList.remove('hfuDropHereFocus');
 	  }
 
-	  if(hfuContainerDiv.classList.contains('hfuContainerDivFocus')){
+	  if(hfuContainerDiv.classList.contains('hfuContainerDivFocus')){//remove .hfuContainerDivFocus from hfuContainerDiv after DROP
 	  	hfuContainerDiv.classList.remove('hfuContainerDivFocus');
 	  }
     }
@@ -97,4 +106,8 @@ function hfu(config){
   });
 
 //END events  
+}
+//
+function draw_file(file,hfuFilesList){
+
 }
--- index.php
+++ index.php
@@ -3,22 +3,21 @@
 <head>
 <meta charset='utf-8'>
 <meta name="viewport" content="width=device-width,initial-scale=1">
-<link href="bootstrap-3.3.7-dist/css/bootstrap.min.css" rel="stylesheet">
 <link href="hassen_fileuploader.css" rel="stylesheet">
 
 <!-- styles of stories-->
 
 </head>
 <body>	
-<input type="file" name='fileinput_prototype' multiple="multiple" id="fileinput_prototype" >
+<input id='myFileinput' type="file" name='myFileinput' multiple="multiple" >
 <script src="hassen_fileuploader.js">
 </script>
 <script>
 hfu({
-	'hfuFileinputPrototype' : "#fileinput_prototype",
-	   "lang" : "fr",
-	   "hfuContainerDivClass" : "myContainerClass",
-	   "hfuDropHereClass" : "myDropHereClass",
+	'hfuFileinputPrototype' : "#myFileinput",
+	"lang" : "en",
+	"hfuContainerDivClass" : "myContainerClass",
+	"hfuDropHereClass" : "myDropHereClass",
 });
 </script>
 </body>
